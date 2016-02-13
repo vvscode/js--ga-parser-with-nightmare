67278e2d36e99bd2d68a45824d6dbc2298fd87c8
Vasil Vanchuk <v.vanchuk@tut.by>
2016-02-13 19:42:48 +0300

    Parse names/emails

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var Nightmare = require('nightmare');
 var vo = require('vo');
 
-var nm = Nightmare({show: true});
-const initialUrl = 'http://github-awards.com/users?utf8=%E2%9C%93&type=city&language=javascript&city=San+francisco';
+var nm = Nightmare({ show: true });
+const initialUrl = 'http://github-awards.com/users?utf8=%E2%9C%93&type=city&language=css&city=Minsk';
 var profiles = [];
+var emails = [];
 
 vo(function* () {
   var hasNextPage;
   var pageProfiles;
-   yield nm.goto(initialUrl);
+  var name;
+  var email;
+  yield nm.goto(initialUrl);
 
   do {
     pageProfiles = yield nm
@@ -19,20 +22,37 @@ vo(function* () {
 
     console.log(`${currentUrl} ${pageProfiles.length}`);
     profiles = profiles.concat(pageProfiles);
-    yield nm
-      .scrollTo(9999, 0)
-      .click('a[rel="next"]')
-      .wait(1000);
 
     hasNextPage = yield nm.exists('a[rel="next"]');
-  } while(hasNextPage);
+    if (hasNextPage) {
+      yield nm
+        .scrollTo(9999, 0)
+        .click('a[rel="next"]')
+        .wait(1000);
+    }
+  } while (hasNextPage);
+
+  for (var i = 0; i <= profiles.length; i++) {
+    var item = profiles[i] || 'vvscode';
+    yield nm.goto(`http://github.com/${item}`);
+    email = yield nm.evaluate(function() {
+      return (document.querySelector('.email') || { text: '' }).text;
+    });
+    name = yield nm.evaluate(function() {
+      return (document.querySelector('.vcard-fullname') || { innerHTML: '' }).innerHTML;
+    });
+    if (email) {
+      emails.push(`${name} (${item});${email}`);
+    }
+  }
 
   yield nm.end();
 })(function(err, result) {
-  if(err) {
+  if (err) {
     return console.log(err);
   }
-  console.log(result);
-  console.log(profiles.join('\n'));
+  //console.log(result);
+  //console.log(profiles.join('\n'));
+  console.log(emails.join('\n'));
 });
 
